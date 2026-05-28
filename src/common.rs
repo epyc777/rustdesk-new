04e836cac280c6dbebd97a2ff039b4fd4e2ddf4d
Tony <tonyzhou711@gmail.com>
2026-05-28 09:36:03 +0800

    Update custom server configurations

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://qq.iw2u.cn:21114".to_owned()
+    "http://rustdesk.146650.xyz:21114".to_owned()
 }
 
 #[inline]
